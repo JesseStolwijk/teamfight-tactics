de8eb07c4062bb26e800645dc76b5572c37e8f46
Jesse Stolwijk <stolwijkjesse@gmail.com>
2020-12-21 23:35:11 +0100

    Test endpoint

--- web-ui/pages/api/hello.js
+++ web-ui/pages/api/hello.js
@@ -1,6 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+import { BACKEND_BASE_URL } from "../../backend/config";
+
 export default (req, res) => {
-  res.statusCode = 200
-  res.json({ name: 'John Doe' })
-}
+  res.statusCode = 200;
+  res.json({
+    url: BACKEND_BASE_URL,
+    accessKeyId: process.env.ACCESS_KEY,
+    secretAccessKey: process.env.SECRET_KEY,
+    TableName: process.env.TABLE_NAME,
+  });
+};
