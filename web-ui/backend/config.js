e5eba7d898ad2764bc73ba4938ced6f1d72a7e05
Jesse Stolwijk <stolwijkjesse@gmail.com>
2020-12-21 23:43:27 +0100

    Fix url

--- web-ui/backend/config.js
+++ web-ui/backend/config.js
@@ -1,2 +1,4 @@
 export const RIOT_API_KEY = process.env.RIOT_API_KEY;
-export const BACKEND_BASE_URL = process.env.NEXT_PUBLIC_VERCEL_URL || "http://localhost:3000";
+export const BACKEND_BASE_URL = process.env.NEXT_PUBLIC_VERCEL_URL
+  ? "https://" + process.env.NEXT_PUBLIC_VERCEL_URL
+  : "http://localhost:3000";
